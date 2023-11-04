dff63287571c4a3263bcc863f8e1d9f896706c4c
notforsilent <notforsilent@gmail.com>
2023-11-04 21:40:55 +0800

    berkas baru:     akmal_dzaki/Js12/akar.java 	berkas baru:     akmal_dzaki/Js12/bunga.java 	berkas baru:     akmal_dzaki/Js12/daun.java 	berkas baru:     akmal_dzaki/Js12/js12.java 	berkas baru:     akmal_dzaki/Js12/tanaman.java 	berkas baru:     akmal_dzaki/Js5/js5.java

---
+++ akmal_dzaki/Js12/daun.java
@@ -0,0 +1,10 @@
+package akmal_dzaki.Js12;
+
+public class daun extends tanaman{
+    void fotosintesis(){
+        nutrisi ++;
+        System.out.println("");
+        System.out.println("Fotosintesis terjadi");
+        System.out.println(nutrisi);
+    }
+}
