2053730404c5de268239437b680f422b22897134
notforsilent <notforsilent@gmail.com>
2023-11-06 14:59:33 +0800

    berkas baru:     studi_kasus/bngun_ruang/limas.java 	berkas baru:     studi_kasus/luas_bngun_datar/jajar_gen.java 	berkas baru:     studi_kasus/luas_bngun_datar/ling.java 	berkas baru:     studi_kasus/luas_bngun_datar/main.java 	berkas baru:     studi_kasus/luas_bngun_datar/persegi.java

---
+++ studi_kasus/luas_bngun_datar/persegi.java
@@ -0,0 +1,10 @@
+package studi_kasus.luas_bngun_datar;
+
+public class persegi extends jajar_gen {
+    float s;
+
+    public float persegi(float s){
+        float hasil = s*s;
+        return hasil;
+    }
+}
