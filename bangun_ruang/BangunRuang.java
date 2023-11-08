05da1815f361389424ad19e08e2ff14538522b55
Lutfi Alwan <112716513+AlwanLutfi@users.noreply.github.com>
2023-11-08 10:09:37 +0800

    Add files via upload

---
+++ bangun_ruang/BangunRuang.java
@@ -0,0 +1,9 @@
+package bangun_ruang;
+
+public class BangunRuang {
+    protected double volume;
+
+    public double getVolume() {
+        return volume;
+    }
+}
---
+++ bangun_ruang/Limas.java
@@ -0,0 +1,15 @@
+package bangun_ruang;
+
+public class Limas extends BangunRuang {
+    double sisi, t;
+
+    public Limas(double sisi, double t) {
+        this.sisi = sisi;
+        this.t = t;
+        Volume();
+    }
+
+    private void Volume() {
+        super.volume = (this.sisi * this.sisi * 1 / 3 * this.t);
+    }
+}
